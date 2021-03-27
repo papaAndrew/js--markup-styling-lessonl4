75cf12ca9070acc3ad5c204aabab83f6a1d53fb2
papaAndrew <a.hermit71@gmail.com>
2021-03-28 01:50:54 +0500

    prettier write and lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: ["airbnb-base"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  rules: {},
+  rules: {
+    quotes: "off",
+  },
 };
